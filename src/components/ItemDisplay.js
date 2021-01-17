b00ad70d01161d61b3439cfa74b91d958d237e6a
alexanderzhong <alexhyzhong@gmail.com>
2021-01-17 13:26:25 -0500

    some more minor changes

--- src/components/ItemDisplay.js
+++ src/components/ItemDisplay.js
@@ -4,20 +4,32 @@ import * as IoIcons from 'react-icons/io5';
 
 export class ItemDisplay extends Component {
     render() {
-        return (
-            <div className="display">
-                <div id="display-header">
-                    <IoIcons.IoCaretBack id="back" onClick={this.props.onBack}/>
-                    <h1 id="display-title">{this.props.data.title}</h1>
+        if (this.props.data.id == 1) {
+            return (
+                <div className="display">
+                    <div id="display-header">
+                        <IoIcons.IoCaretBack id="back" onClick={this.props.onBack}/>
+                        <h1 id="display-title">{this.props.data.title}</h1>
+                    </div>
+                    <p id="display-description">{this.props.data.description}</p>
+                    <center><iframe id="demo" width="560" height="315" 
+                    src="https://www.youtube.com/embed/qkdmO3_Z-Rs" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen></iframe></center>
                 </div>
-                <p id="display-description">{this.props.data.description}</p>
-                <center><iframe id="demo" width="560" height="315" 
-                src="https://www.youtube.com/embed/qkdmO3_Z-Rs" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen></iframe></center>
-            </div>
-        )
+            );
+        } else {
+            return (
+                <div className="display">
+                    <div id="display-header">
+                        <IoIcons.IoCaretBack id="back" onClick={this.props.onBack}/>
+                        <h1 id="display-title">{this.props.data.title}</h1>
+                    </div>
+                    <p id="display-description">{this.props.data.description}</p>
+                </div>
+            );
+        }
     }
 }
 
